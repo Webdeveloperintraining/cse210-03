9a03b927677d5b01d455f70709b723464232114f
blinkythefish <pottle.jamin@gmail.com>
2022-10-17 18:51:15 -0600

    set up the basic lay out of the director class

--- director.py
+++ director.py
@@ -3,3 +3,33 @@ file: director.py
 
 this file houses the director class that is in charge of the game operations
 '''
+
+class Director():
+    '''
+        This class has the responsibility to run all other classes as well as make the game work properly
+        as stated in the program requirements. 
+	'''
+
+    def __init__(self):
+        pass
+
+    def start_game(self):
+        '''
+            This function is in charge of starting the game and running the game
+        '''
+
+    
+    def _get_inputs(self):
+        '''
+            this function gets input from the user and validates it
+        '''
+
+    def _update_game(self):
+        '''
+            updates the game board
+        '''
+
+    def _show_output(self):
+        '''
+            once game has been updated the output can then be shown to the user
+        '''
