1de65355ecde923cedcdd8d26595d30c0b51c936
blinkythefish <pottle.jamin@gmail.com>
2022-10-19 19:51:16 -0600

    added main.py to run game, added input with player_input class

--- director.py
+++ director.py
@@ -4,6 +4,9 @@ file: director.py
 this file houses the director class that is in charge of the game operations
 '''
 
+
+from player_input import Player_Input
+
 class Director():
     '''
         This class has the responsibility to run all other classes as well as make the game work properly
@@ -11,18 +14,25 @@ class Director():
 	'''
 
     def __init__(self):
-        pass
+        self._playing_game = True
+        self._terminal_service = Player_Input()
 
     def start_game(self):
         '''
             This function is in charge of starting the game and running the game
         '''
+        while self._playing_game:
+            self._get_inputs()
+            self._update_game()
+            self._show_output()
 
     
     def _get_inputs(self):
         '''
             this function gets input from the user and validates it
         '''
+        user_input = self._terminal_service.input_in_hidden_word()
+        print(user_input)
 
     def _update_game(self):
         '''
---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+    Runs the director class which runs the game
+'''
+
+from director import Director
+
+game_director = Director()
+
+game_director.start_game()
