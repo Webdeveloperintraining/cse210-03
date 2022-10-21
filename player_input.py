0645cfd3d989a8602938927cf03ec1b0529d3bbf
Webdeveloperintraining <sil22006@byui.edu>
2022-10-20 17:55:53 -0700

    Quick fix for this class

--- player_input.py
+++ player_input.py
@@ -6,37 +6,37 @@ class Player_Input():
 	'''
     def __init__(self,hidden_word=["c","a","t"]):
         self.hidden_letters=hidden_word
-        self._player_input=input("Guess a letter [a-z]: ").lower()
         self._words_used=[]
     
     def input_in_hidden_word(self):
         '''
         This method verifies if the player's input is a letter from the the hidden word.
         '''
-        self.verify_not_repeated_word()
+        player_input=input("Guess a letter [a-z]: ").lower()
+        self.verify_not_repeated_word(player_input)
 
         for i in self.hidden_letters:
 
-            if i in self._player_input:
+            if i in player_input:
                 return i
 
         return False
 
-    def verify_not_repeated_word(self):
+    def verify_not_repeated_word(self,player_input):
         '''
         This method verifies there isn't a letter that has already already typed by the player.
         '''
-        while self._player_input in self._words_used:
+        while player_input in self._words_used:
             self._player_input=input("Guess a letter [a-z]: ").lower()
-        if self._player_input not in self._words_used:
-            self._words_used.append(self._player_input)
+        if player_input not in self._words_used:
+            self._words_used.append(player_input)
 
     def clear_words_used_list(self):
         self._words_used.clear()
-        #print(self._words_used)
+        print(self._words_used)
 
-# test=Player_Input()
-# test.input_in_hidden_word()
-# test.input_in_hidden_word()
-# print(test._words_used)
-# test.clear_words_used_list()
+test=Player_Input()
+test.input_in_hidden_word()
+test.input_in_hidden_word()
+print(test._words_used)
+test.clear_words_used_list()
