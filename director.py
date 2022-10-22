c7d4a958e8f4fbaa611f0626f5f479e704aa1157
blinkythefish <pottle.jamin@gmail.com>
2022-10-21 18:00:19 -0600

    added letters_output to director class

--- director.py
+++ director.py
@@ -7,6 +7,7 @@ this file houses the director class that is in charge of the game operations
 from player_input import Player_Input
 from words_randomizer import WordRandomizer
 from draw_jumper import DrawJumper
+from letters_output import Letters_output
 
 class Director():
     '''
@@ -18,9 +19,13 @@ class Director():
         self._playing_game = True
         self._wrong_guesses = 0
         self._correct_guess = False
-        self._random_word = WordRandomizer()
-        self._terminal_service = Player_Input(self._random_word.get_word())
+        self._player_input = ''
+
+        self._random_word_gen = WordRandomizer()
+        self._random_word = self._random_word_gen.get_word()
+        self._terminal_service = Player_Input(self._random_word)
         self._draw_jumper = DrawJumper()
+        self._word_output = Letters_output(self._random_word)
 
 
     def start_game(self):
@@ -29,6 +34,7 @@ class Director():
         '''
         print('starting game')
         self._show_output()
+        # self._word_output.clear_underscore(self._player_input)
 
         
         while self._playing_game:
@@ -49,6 +55,7 @@ class Director():
         user_input = self._terminal_service.input_in_hidden_word()
         if user_input:
             self._correct_guess = True
+            self._player_input = user_input
 
         else:
             self._correct_guess = user_input
@@ -59,18 +66,28 @@ class Director():
             updates the game board
         '''
 
+
         if not self._correct_guess:
             self._wrong_guesses = self._wrong_guesses + 1
 
         if self._wrong_guesses >= 4:
             self._playing_game = False
+            print('\n\nYou lost...')
+            
 
     def _show_output(self):
         '''
             once game has been updated the output can then be shown to the user
         '''
         print(self._wrong_guesses)
+
         self._draw_jumper.print_jumper(self._wrong_guesses)
+
+        player_not_won = self._word_output.print_places(self._player_input)
+
+        if not player_not_won:
+            self._playing_game = False
+            print('\n\nYou WON!')
 
 
     def _play_again(self):
@@ -78,12 +95,19 @@ class Director():
         while True:
             play_again = input('Would you like to play again? (y, n): ').strip().lower()
             if play_again == 'y':
+
                 self._playing_game = True
                 self._wrong_guesses = 0
                 self._correct_guess = False
-                self._terminal_service.hidden_letters = self._random_word.get_word()
+                self._player_input = ''
+
+                self._random_word = self._random_word_gen.get_word()
+
+                self._terminal_service.hidden_letters = self._random_word
                 self._terminal_service.clear_words_used_list()
+                # self._word_output.clear_underscore()
+                self._word_output.clear_underscore(self._random_word)
                 self.start_game()
             else:
                 print('Good bye!')
-                return False
+                break
--- letters_output.py
+++ letters_output.py
@@ -18,20 +18,42 @@ class Letters_output():
     
     def replace_underscores (self, match_letter):
 
-        is_there = isinstance(match_letter, str)
-        
-        if is_there:
-            if match_letter == False or match_letter == "":
-                pass
-            else:
-                index = self.letters_list.index(match_letter)
+        # is_there = isinstance(match_letter, str)
+        # if is_there:
+        index = 0
+        for letter in self.letters_list:
+            if match_letter == letter:
                 self.underscores[index] = match_letter
+            index += 1
+
+            # if match_letter == False or match_letter == "":
+            #     pass
+            # else:
+            #     index = self.letters_list.index(match_letter)
+            #     self.underscores[index] = match_letter
 
     def print_places (self, match_letter):
-        self.appends_underscores()
+        if len(self.underscores) == 0:
+            self.appends_underscores()
         self.replace_underscores(match_letter)
         print(*self.underscores)
+        
+        return self.check_win()
 
+    def clear_underscore(self, new_word):
+        self.underscores.clear()
+        self.letters_list = new_word
+        self.word_length = len(self.letters_list)
+        self.lits_length = len(self.underscores)
+    
+    def check_win(self):
+        still_underscore = True
+        for letter in self.underscores:
+            if letter == '_':
+                still_underscore = True
+                return still_underscore
+
+        return False
 
 
 
